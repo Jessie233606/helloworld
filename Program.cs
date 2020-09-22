7346c13fc0a641f8fa48c50c6837cdf9a970cf8f
Jessie233606 <71690744+Jessie233606@users.noreply.github.com>
2020-09-23 00:02:32 +0800

    第一次修改

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace helloworld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("this is my first line");
+            
         }
     }
 }
