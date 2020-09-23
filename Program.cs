4b75776ecd465f27aead764fea18172504bdc0c1
Jessie233606 <71690744+Jessie233606@users.noreply.github.com>
2020-09-23 21:50:39 +0800

    加入了一个判断语句

--- Program.cs
+++ Program.cs
@@ -6,9 +6,16 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
+            int a=5;
+            if(a<2)
+            {
             Console.WriteLine("Hello World!");
+            }
+            else
+            {
             Console.WriteLine("this is my first line");
-            
+            Console.Beep(1000,1000);//发出一个蜂鸣
+            }
         }
     }
 }
